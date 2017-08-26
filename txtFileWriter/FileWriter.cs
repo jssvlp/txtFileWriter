bacbf911b393e31509d464cb8252b40fde019c32
Jossias Velazquez Peña <jossias.vel@gmail.com>
2017-08-26 17:14:39 -0400

    First Commit
    Biblioteca de clases para escribir archivos txt

---
+++ txtFileWriter/FileWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace txtFileWriter
+{
+    public class FileWriter
+    {
+        private string path;
+        private string texto;
+
+
+        public FileWriter()
+        {
+            this.path = texto;
+        }
+        public void EscribirArchivo(string texto )
+        {
+
+            try {
+                if (!File.Exists(path))
+                {
+                    File.Create(path);
+                    StreamWriter sw = new StreamWriter(path);
+                    sw.WriteLine(texto + " " + DateTime.Now);
+                    Console.WriteLine("Archivo creado y escrito");
+                    sw.Close();
+                }
+                else
+                {
+                    StreamWriter sw = File.AppendText(path);
+                    sw.WriteLine(texto + " " + DateTime.Now);
+                    sw.Close();
+                }
+                
+            }
+            catch (IOException ex) {
+                throw ex;
+            }
+        }
+    }
+}
